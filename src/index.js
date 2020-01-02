615d4d2dd223db50a4d925ad1d6ff7e2294ddf47
Keehada <frankgquijada@hotmail.com>
2020-01-02 15:28:59 -0600

    added index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './Components/Header';
-import NavLink from 'react-bootstrap/NavLink'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import HomePage from './Components/HomePage.js';
+import Projects from './Components/Projects.js';
+import Jumbotron from './Components/Jumbotron.js';
 
 function App() {
     return (
         <Router>
             <Header/>
+            <Jumbotron/>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/Projects' component={Projects}/>
         </Router>
-    )
+    ) 
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
